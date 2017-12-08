230e955f0b1393c5f30e6e0a22fea6f04b1babd7
kryx07 <kryx07@gmail.com>
2017-12-08 20:57:05 +0100

    Lucene temporarily removed

--- src/main/java/com/krystiankowalik/pdfsearchengine/Main.kt
+++ src/main/java/com/krystiankowalik/pdfsearchengine/Main.kt
@@ -2,7 +2,7 @@ package com.krystiankowalik.pdfsearchengine
 
 fun main(args: Array<String>) {
 
-    SimplePDFSearch().run()
+    print(SimplePDFSearch("/home/wd43/Downloads/testdskjfd/kp.pdf").containsStrings(listOf("ODeKs","kodeks")))
 
 
 }
--- src/main/java/com/krystiankowalik/pdfsearchengine/SimplePDFSearch.kt
+++ src/main/java/com/krystiankowalik/pdfsearchengine/SimplePDFSearch.kt
@@ -4,42 +4,16 @@ import org.apache.pdfbox.pdmodel.PDDocument
 import org.apache.pdfbox.text.PDFTextStripper
 import java.io.File
 
-class SimplePDFSearch {
+class SimplePDFSearch(val pdfFilePath: String) {
 
-
-    companion object {
-        val INDEX_DIR = "/home/wd43/Downloads/testdskjfd/index/"
-        val DEFAULT_RESULT_SIZE = 100
+    fun containsStrings(stringsToQuery: List<String>): Map<String, Boolean> {
+        val pdfFile = File(pdfFilePath)
+        val documentContents = extractTextFromPdfFile(pdfFile).toLowerCase()
+        return stringsToQuery.map {
+            it to documentContents.contains(it.toLowerCase())
+        }.toMap()
     }
 
-    fun run() {
-        val pdfFile = File("/home/wd43/Downloads/testdskjfd/kp.pdf")
-        val pdfIndexItem = index(pdfFile)
-
-        val indexer = Indexer(INDEX_DIR)
-        indexer.index(pdfIndexItem)
-        indexer.close()
-
-        val searcher = Searcher(INDEX_DIR)
-        val result = searcher.findByContent("KoDeKs", DEFAULT_RESULT_SIZE)
-        printResult(result)
-        searcher.close()
-    }
-
-    fun index(file: File): IndexItem {
-        val pdDocument = PDDocument.load(file)
-        val content = PDFTextStripper().getText(pdDocument)
-        pdDocument.close()
-        return IndexItem(file.name.hashCode().toLong(), file.name, content)
-    }
-
-    fun printResult(result: Int) {
-        if (result == 1) {
-            print("Contains")
-        } else {
-            println("Doesn't containt keyword")
-        }
-    }
-
-
+    fun extractTextFromPdfFile(pdfFile: File): String =
+            PDFTextStripper().getText(PDDocument.load(pdfFile))
 }
